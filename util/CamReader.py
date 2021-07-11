184984c7e5735e483b3316dbc1eab46d46294892
Trisanu <trisanubhar@gmail.com>
2021-07-12 00:18:43 +0530

    Added continuous photo effect

--- util/CamReader.py
+++ util/CamReader.py
@@ -1,8 +1,11 @@
-import cv2 as cv
-from sklearn.cluster import KMeans
-import numpy as np
 import random
 import string
+from os import system
+from time import sleep
+
+import cv2 as cv
+import numpy as np
+from sklearn.cluster import KMeans
 
 #  0 -> camera number, if external camera is installed this number needs to be changed
 cap = cv.VideoCapture(0)
@@ -13,11 +16,8 @@ if not cap.isOpened():
     exit()
 
 
-def _capture_image_as_ascii():
-    """
-
-    :return: reads the camera
-    """
+def _capture_image_as_ascii() -> None:
+    """:return: reads the camera"""
     count = 0
     while True:
         # Capture frame-by-frame
@@ -38,7 +38,7 @@ def _capture_image_as_ascii():
         # frame is the coloured image
         #  gray is the grayscaled version
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
+        print(type(gray))
         # @dha do the operation of converting a np object to asciimate object here
         _print_to_dom_color(gray, 7)
 
@@ -50,36 +50,44 @@ def _capture_image_as_ascii():
             break
 
 
-def _print_to_dom_color(gray, no_of_colors):
-    """
+def _print_to_dom_color(gray: np.ndarray, no_of_colors: int) -> None:
+    """Function for printing photo to the screen in ascii
 
     :param gray: gray scale image, size doesn't matter
     :param no_of_colors: number of colours you want to reduce your image to
     :return:
     """
-
     flat_gray = gray.reshape([gray.shape[0] * gray.shape[1], 1])
 
     kmeans = KMeans(n_clusters=no_of_colors)
     kmeans.fit(flat_gray)
 
     colors = kmeans.cluster_centers_.astype('float')
-    flat_im_dom_color = colors[abs(np.array(flat_gray[None, :], dtype=float) - colors[:, None]).argmin(axis=0)]
-    im_dom_color = np.array(flat_im_dom_color.reshape([gray.shape[0], gray.shape[1]]), dtype='uint8')
+    flat_im_dom_color = colors[abs(
+        np.array(flat_gray[None, :], dtype=float) - colors[:, None]).argmin(axis=0)]
+    im_dom_color = np.array(flat_im_dom_color.reshape(
+        [gray.shape[0], gray.shape[1]]), dtype='uint8')
     np.where(im_dom_color)
     # print(im_dom_color.shape)
     # cv.imshow(im_dom_color)
 
     # r = lambda: random.randint(0,255)
     # asciis = np.array(["%06x" % random.randint(0, 0xFFFFFF) for n in range(no_of_colors)])
-    asciis = np.array([random.choice(string.punctuation) for n in range(no_of_colors)])
-    flat_im_dom_color = asciis[abs(np.array(flat_gray[None, :], dtype=float) - colors[:, None]).argmin(axis=0)]
-    im_dom_color = np.array(flat_im_dom_color.reshape([gray.shape[0], gray.shape[1]]), dtype='str')
+    asciis = np.array(
+        [random.choice(string.punctuation) for n in range(no_of_colors)])
+    flat_im_dom_color = asciis[abs(
+        np.array(flat_gray[None, :], dtype=float) - colors[:, None]).argmin(axis=0)]
+    im_dom_color = np.array(flat_im_dom_color.reshape(
+        [gray.shape[0], gray.shape[1]]), dtype='str')
     # print(im_dom_color.shape)
     # print(asciis)
     with np.printoptions(threshold=np.inf):
-        print('\n'.join(''.join(str(cell) for cell in row) for row in im_dom_color))
-    print("\n\n\n\n\n\n")
+        print('\n'.join(''.join(str(cell) for cell in row)
+                        for row in im_dom_color))
+    # print("\n\n\n\n\n\n")
+    # Added this for the continous effect, might have to change 0.5 to frame rate later
+    sleep(0.5)  # noqa: S605
+    system("clear")
 
 
 # When everything done, release the capture
