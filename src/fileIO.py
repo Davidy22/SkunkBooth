a62c1315ad3186917d2d6d985ebb7cdbc18d6118
David Yang <davidyang6us@gmail.com>
2021-07-13 20:47:32 +0800

    12x performance on 4000 frames rendering ASCII to image (#17)
    * 12x performance on 4000 frames rendering ASCII to image
    
    * Perf improvements, fixed character height issue, added attribute support

--- src/fileIO.py
+++ src/fileIO.py
@@ -5,16 +5,30 @@ from cv2 import VideoWriter, VideoWriter_fourcc
 from numpy import array
 from PIL import Image, ImageDraw, ImageFont
 
+from data import constants
+
 
 class IOBase:
     """ASCII conversion module, subclass for access to convert()"""
 
     def __init__(self):
-        self.colours = load(open("data/colours.json"))
         self.font = ImageFont.truetype("data/Input.ttf", 30)  # TODO: Font config
-        self.fx, self.fy = self.font.getsize(" ")
-        self.lookup = {}
-        self.charBack = Image.new("RGB", (self.fx, self.fy))
+        self.fx, self.fy = self.font.getsize("g")
+        self.glyphs = {}
+        self.renderCache = {}
+        self.maxCache = 5000
+
+        cLookup = load(open("data/colours.json"))
+        self.colours = {i: Image.new("RGB", (self.fx, self.fy)) for i in range(256)}
+        for i in self.colours:
+            self.colours[i].paste(
+                tuple(cLookup[str(i)]["rgb"].values()), (0, 0, self.fx, self.fy)
+            )
+        self.underline = Image.new("RGBA", (self.fx, self.fy))
+        ImageDraw.Draw(self.underline).line(
+            (0, self.fy - self.fy // 11, self.fx, self.fy - self.fy // 11),
+            width=self.fy // 8,
+        )
 
     def convert(self, image: List[List[Tuple[int, int, int]]]) -> array:
         """
@@ -31,34 +45,41 @@ class IOBase:
                 # TODO: Implement effects parameter
                 # TODO: Implement background color
                 char = chr(image[y][x][0])
-                if char not in self.lookup:
+                if char not in self.glyphs:
                     # Cache character glyphs
                     glyph = Image.new("RGBA", (self.fx, self.fy))
-                    ImageDraw.Draw(glyph).text(
-                        (x * self.fx, y * self.fy),
-                        char,
-                        (255, 255, 255),
-                        font=self.font,
-                    )
-                    glyph = array(glyph)
-                    r, g, b, _ = glyph.T
-                    self.lookup[char] = (
-                        glyph,
-                        ((r == 255) & (g == 255) & (b == 255)).T,
-                    )
+                    ImageDraw.Draw(glyph).text((0, 0), char, font=self.font)
+                    self.glyphs[char] = glyph
 
-                # Recolor character
-                char = self.lookup[char]
-                char[0][..., :-1][char[1]] = tuple(
-                    self.colours[str(image[y][x][2])]["rgb"].values()
-                )
-                char = Image.fromarray(char[0])
+                # Render character, (text colour, bg colour, char, attribute)
+                charD = (image[y][x][2], 0, char, image[y][x][1])
+                if charD in self.renderCache:
+                    render = self.renderCache[charD]
+                else:
+                    fg = self.colours[charD[0]]
+                    bg = self.colours[charD[1]]
+                    glyph = self.glyphs[charD[2]]
 
-                # Render character
-                out.paste(
-                    Image.composite(char, self.charBack, char),
-                    (x * self.fx, y * self.fy),
-                )
+                    # Attributes
+                    if charD[3] in constants.L_BOLD:
+                        if charD[2] + "bold" in self.glyphs:
+                            glyph = self.glyphs[charD[2] + "bold"]
+                        else:
+                            temp = glyph.copy()
+                            temp.paste(glyph, (self.fx // 20, 0), glyph)
+                            temp.paste(glyph, (0, self.fy // 20), glyph)
+                            glyph = self.glyphs[charD[2] + "bold"] = temp
+                    if charD[3] in constants.L_REVERSE:
+                        fg, bg = bg, fg
+                    if charD[3] in constants.L_UNDERLINE:
+                        glyph = glyph.copy()
+                        glyph.paste(self.underline, (0, 0), self.underline)
+                    render = self.renderCache[charD] = Image.composite(fg, bg, glyph)
+
+                    if len(self.renderCache) > self.maxCache:
+                        self.renderCache.pop(next(iter(self.renderCache)))
+
+                out.paste(render, (x * self.fx, y * self.fy))
         return array(out)
 
 
@@ -93,8 +114,21 @@ class videoIO(IOBase):  # TODO: Other video filetypes
 if __name__ == "__main__":
     import random
 
-    v = videoIO((10, 10))
-    for _ in range(400):
-        f = [[(102, 2, random.randint(0, 255)) for _ in range(10)] for _ in range(10)]
-        v.write(f)
+    v = videoIO((20, 20))
+    [
+        v.write(
+            [
+                [
+                    (
+                        random.randint(97, 122),
+                        random.randint(1, 8),
+                        (i + (j + 1) * (k + 1)) % 255,
+                    )
+                    for j in range(20)
+                ]
+                for k in range(20)
+            ]
+        )
+        for i in range(400)
+    ]
     v.close()
