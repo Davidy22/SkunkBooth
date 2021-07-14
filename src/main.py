9d07b35ced474ab08ccb4dca45fb647a45a8b1d1
Shriram <shriramraghav1998@gmail.com>
2021-07-14 23:16:03 +0530

    Gallery frame added

--- src/main.py
+++ src/main.py
@@ -2,14 +2,16 @@ import os
 import sys
 from typing import Any
 
-from asciimatics.exceptions import ResizeScreenError, StopApplication
+from asciimatics.exceptions import (
+    NextScene, ResizeScreenError, StopApplication
+)
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
-from asciimatics.widgets import Button, Frame, Layout
+from asciimatics.widgets import Button, FileBrowser, Frame, Label, Layout
 
 from logger import CustomLogger
 
-logger = CustomLogger(fileoutpath="Logs" + os.sep + "ui.log")
+logger = CustomLogger(fileoutpath="src/Logs" + os.sep + "ui.log")
 
 
 class MainFrame(Frame):
@@ -23,32 +25,24 @@ class MainFrame(Frame):
                                         hover_focus=True,
                                         can_scroll=False,
                                         title="Photobooth")
-        self._gallery_button = Button("Gallery", self._gallery, add_box=True)
-        self._effects_button = Button("Effects", self._effects, add_box=True)
-        self._camera_button = Button(u"📷 Camera", self._shoot, add_box=True)
+        self._gallery_button = Button("🖼 Gallery", self._gallery, add_box=True)
+        self._effects_button = Button("🖌 Effects", self._effects, add_box=True)
+        self._camera_button = Button(u"📷 Take a picture", self._shoot, add_box=True)
         self._video_recording = Button(u"⏯︎  Rec. Start/Stop", self._start_stop_recording, add_box=True)
-        self._quit_button = Button("Quit", self._quit, add_box=True)
+        self._quit_button = Button("⛌ Quit", self._quit, add_box=True)
         camera_layout = Layout([100], fill_frame=True)
         self.add_layout(camera_layout)
         controls_layout = Layout([1, 1, 1, 1, 1])
         self.add_layout(controls_layout)
         controls_layout.add_widget(self._gallery_button, 0)
-        controls_layout.add_widget(self._camera_button, 1)
-        controls_layout.add_widget(self._video_recording, 2)
+        controls_layout.add_widget(self._video_recording, 1)
+        controls_layout.add_widget(self._camera_button, 2)
         controls_layout.add_widget(self._effects_button, 3)
         controls_layout.add_widget(self._quit_button, 4)
         self.set_theme("monochrome")
         self.fix()
 
         logger._log_info("Mainframe initialized")
-
-    def _gallery(self) -> None:
-        """Open gallery"""
-        logger._log_info("Gallery was clicked")
-
-    def _shoot(self) -> None:
-        """Take an image"""
-        logger._log_info("Camera was clicked")
 
     def _effects(self) -> None:
         """Open effects"""
@@ -59,15 +53,62 @@ class MainFrame(Frame):
         logger._log_info("Recording started/stopped")
 
     @staticmethod
+    def _gallery() -> None:
+        """Open gallery"""
+        logger._log_info("Gallery was clicked")
+        raise NextScene("Gallery")
+
+    @staticmethod
+    def _shoot() -> None:
+        """Take an image"""
+        logger._log_info("Camera was clicked")
+
+    @staticmethod
     def _quit() -> None:
         """Quit application"""
         logger._log_info("Application was stopped")
         raise StopApplication("User pressed quit")
 
 
+class GalleryFrame(Frame):
+    """Recreatable frame to implement gallery ui"""
+
+    def __init__(self, screen: Any) -> None:
+        """Initialize frame"""
+        super(GalleryFrame, self).__init__(screen,
+                                           screen.height,
+                                           screen.width,
+                                           hover_focus=True,
+                                           can_scroll=False,
+                                           title="Photobooth")
+        self._back_camera_button = Button(u"🠔 Back to 📷", self._switch_to_camera, add_box=True)
+        self._browser = FileBrowser(screen.height//2, "src/data")
+        title_layout = Layout([1])
+        self.add_layout(title_layout)
+        files_layout = Layout([100], fill_frame=True)
+        self.add_layout(files_layout)
+        controls_layout = Layout([1, 1, 1])
+        self.add_layout(controls_layout)
+        title_layout.add_widget(Label("Gallery", align="^", height=screen.height//16))
+        files_layout.add_widget(self._browser)
+        controls_layout.add_widget(self._back_camera_button, 1)
+        self.set_theme("monochrome")
+        self.fix()
+
+        logger._log_info("Galleryframe initialized")
+
+    @staticmethod
+    def _switch_to_camera() -> None:
+        """Switch to Camera from Gallery"""
+        logger._log_info("Switched to Camera from Gallery")
+        raise NextScene("Main")
+
+
 def ScreenWrapper(screen: Any, scene: Any) -> None:
     """Add scenes to screen and display"""
-    scenes = [Scene([MainFrame(screen)], -1, name="Main")]
+    scenes = [Scene([MainFrame(screen)], -1, name="Main"),
+              Scene([GalleryFrame(screen)], -1, name="Gallery")
+              ]
 
     screen.play(scenes, stop_on_resize=True, start_scene=scene, allow_int=True)
 
