6a582044c8600f8d863e1acef07194d550ee9938
Rashid Ali <80094039+rashidkalwar@users.noreply.github.com>
2021-08-05 22:36:43 +0500

    Update helpers.py

--- skunkbooth/utils/helpers.py
+++ skunkbooth/utils/helpers.py
@@ -44,3 +44,5 @@ def CamDimensions(height: int, width: int) -> Tuple[int, int, int]:
         height = int(height * 2 / 3)
         width = int(width * 2 / 3)
         return (height, width, 2)
+    
+    
