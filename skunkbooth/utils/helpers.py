1322bea331a8141b7c7b1fe3f2caa430459f64d6
Rashid Ali <80094039+rashidkalwar@users.noreply.github.com>
2021-08-05 22:58:54 +0500

    Update helpers.py

--- skunkbooth/utils/helpers.py
+++ skunkbooth/utils/helpers.py
@@ -21,28 +21,28 @@ def global_shortcuts(event: Event) -> None:
 
 
 def toggleFlag(flag: List[int]) -> None:
-        """Temp function for toggling video recording from inside screen"""
-        vidBuf = Queue(32767)
-        flag[0] = not flag[0]
-        # re-initialize VideoIO for new file name
-        if flag[0]:
-            VID_FILE = f"{PIC_DIR}/Video-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.avi"
-            logging.info(f"Recording new video - {VID_FILE}")
-            vidBuf.put(VID_FILE)
-        else:
-            logging.info("Recording stopped.")
+    """Temp function for toggling video recording from inside screen"""
+    vidBuf = Queue(32767)
+    flag[0] = not flag[0]
+    # re-initialize VideoIO for new file name
+    if flag[0]:
+        VID_FILE = f"{PIC_DIR}/Video-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.avi"
+        logging.info(f"Recording new video - {VID_FILE}")
+        vidBuf.put(VID_FILE)
+    else:
+        logging.info("Recording stopped.")
 
 
 def CamDimensions(height: int, width: int) -> Tuple[int, int, int]:
-        """Calculate dimensions for vertical squeeze screen sizes"""
-        if width / height >= 4:
-            height -= 8
-            var_dim = int(height * 4)  # Max width is around twice height in most cases
-            offset = int(width / 2 - var_dim / 2.5 - width / 5)
-            return (height, var_dim, offset)
-        # Add margins of 1/6x,y if no vertical squeeze
-        height = int(height * 2 / 3)
-        width = int(width * 2 / 3)
-        return (height, width, 2)
+    """Calculate dimensions for vertical squeeze screen sizes"""
+    if width / height >= 4:
+        height -= 8
+        var_dim = int(height * 4)  # Max width is around twice height in most cases
+        offset = int(width / 2 - var_dim / 2.5 - width / 5)
+        return (height, var_dim, offset)
+    # Add margins of 1/6x,y if no vertical squeeze
+    height = int(height * 2 / 3)
+    width = int(width * 2 / 3)
+    return (height, width, 2)
     
     
